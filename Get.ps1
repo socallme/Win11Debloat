71b39b75099dc83f673ee6340b8b8c904d8ad9f9
socallme <140671519+socallme@users.noreply.github.com>
2025-07-20 14:04:19 +1000

    Update Get.ps1

--- Get.ps1
+++ Get.ps1
@@ -79,7 +79,7 @@ Write-Output "------------------------------------------------------------------
 Write-Output "> Downloading Win11Debloat..."
 
 # Download latest version of Win11Debloat from github as zip archive
-Invoke-RestMethod https://api.github.com/repos/Raphire/Win11Debloat/zipball/2025.06.12 -OutFile "$env:TEMP/win11debloat.zip"
+Invoke-RestMethod https://github.com/socallme/Win11Debloat/archive/refs/heads/master.zip -OutFile "$env:TEMP/win11debloat.zip"
 
 # Remove old script folder if it exists, except for CustomAppsList and SavedSettings files
 if (Test-Path "$env:TEMP/Win11Debloat") {
